3ec38941e792983e9b11f40fec8b6e6c252aeba9
“Narendra <narendramaharjan90s@gmail.com>
2017-03-07 17:45:18 +0000

    Created user tables and models

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170128024509) do
+ActiveRecord::Schema.define(version: 20170307173019) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
     t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "username"
+    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
